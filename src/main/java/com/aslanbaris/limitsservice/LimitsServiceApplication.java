bd404f3ecd710cc98e8cb309d3bf0e8950eed8b6
Barış Aslan <aslannbaris@gmail.com>
2020-12-14 23:15:14 +0300

    update

--- src/main/java/com/aslanbaris/limitsservice/LimitsServiceApplication.java
+++ src/main/java/com/aslanbaris/limitsservice/LimitsServiceApplication.java
@@ -3,7 +3,9 @@ package com.aslanbaris.limitsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @EnableConfigurationProperties
 @SpringBootApplication
 public class LimitsServiceApplication {

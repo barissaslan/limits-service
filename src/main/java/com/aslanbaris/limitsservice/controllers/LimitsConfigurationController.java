affa7d46bc858141025a15a641255ac67c5bd5e0
Barış Aslan <aslannbaris@gmail.com>
2020-12-16 23:40:24 +0300

    zipkin

--- src/main/java/com/aslanbaris/limitsservice/controllers/LimitsConfigurationController.java
+++ src/main/java/com/aslanbaris/limitsservice/controllers/LimitsConfigurationController.java
@@ -2,6 +2,7 @@ package com.aslanbaris.limitsservice.controllers;
 
 import com.aslanbaris.limitsservice.Configuration;
 import com.aslanbaris.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,16 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfigurations() {
         return new LimitConfiguration(configuration.getMinimum(), configuration.getMaximum());
+    }
+
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    @GetMapping("/fault-tolerance-example")
+    public LimitConfiguration retrieveConfiguration() {
+        throw new RuntimeException("Not available");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfiguration() {
+        return new LimitConfiguration(1, 2);
     }
 
 }
